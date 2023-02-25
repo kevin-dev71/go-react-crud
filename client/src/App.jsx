769e1e13744554a6f55a82860d1c1e61a40a82c4
Kevin Dev <kevin.developer71@gmail.com>
2023-02-24 22:13:02 -0600

    feat: mogodb and get users endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,41 @@
 // @ts-nocheck
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState('');
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch(import.meta.env.VITE_API_URL + '/users', {
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    fetch(import.meta.env.VITE_API_URL + '/users').then(res => res.json()).then(data => setUsers(data?.data))
+  };
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL + '/users').then(res => res.json()).then(data => setUsers(data?.data))
+  }, [])
+  
+
   return (
     <div className="container">
-      <h1>Hello World</h1>
-      <button
-        onClick={async () => {
-          const response = await fetch('/users');
-          const data = await response.json();
-          setUsers(data.data);
-        }}
-      >
-        Get users
-      </button>
-      <div>{users}</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Insert your name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Save</button>
+      </form>
+
+      {users.map(item => <div key={item._id}>{item.name}</div>)}
     </div>
   );
 }
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/kevin-dev71/go-react-crud/models"
 )
 
 func main() {
@@ -15,11 +21,53 @@ func main() {
 	}
 	app := fiber.New()
 	app.Use(cors.New())
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb"))
+
+	if err != nil {
+		panic(err)
+	}
+
 	app.Static("/", "./client/dist")
 
 	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+
+		coll := client.Database("gomongodb").Collection("users")
+		results, err := coll.Find(context.TODO(), bson.M{})
+
+		if err != nil {
+			panic(err)
+		}
+
+		for results.Next(context.TODO()) {
+			var user models.User
+			results.Decode(&user)
+			users = append(users, user)
+		}
+
 		return c.JSON(&fiber.Map{
-			"data": "users from backend",
+			"data": users,
+		})
+	})
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user models.User
+
+		c.BodyParser(&user)
+
+		coll := client.Database("gomongodb").Collection("users")
+		result, err := coll.InsertOne(context.TODO(), bson.D{
+			{
+				Key: "name", Value: user.Name,
+			},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		return c.JSON(&fiber.Map{
+			"data": result,
 		})
 	})
 	app.Listen(":" + port)
